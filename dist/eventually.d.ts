eaf862b3f63f8362d97f9bf5408b2c23aa78d6d4
dobashi <dobashi@lavans.jp>
2023-04-26 11:06:36 +0900

    update typescript, remove dist/* files

--- dist/eventually.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const eventuallySync: <R>(f: () => R, timeout?: number, interval?: number) => Promise<R>;
-export declare const eventually: <R>(f: () => Promise<R>, timeout?: number, interval?: number) => Promise<R>;
